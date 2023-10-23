d083a5ebf92709ffc4067fe4148c19ad9da748a1
murnana <murnana@users.noreply.github.com>
2023-10-23 23:20:28 +0900

    :sparkles: add ABC325 Results

---
+++ abc325/Rated-AC/A.cs
@@ -0,0 +1,9 @@
+// https://atcoder.jp/contests/abc325/tasks/abc325_a
+
+
+// 入力
+var input = Console.ReadLine()?.Split(' ').First();
+
+//-----------------------------------------------------------------------------
+// 出力
+Console.WriteLine($"{input} san");
